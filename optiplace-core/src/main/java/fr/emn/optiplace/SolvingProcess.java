3f698564cac8e3d36c2b9ae3f6cdaf969a5b445e
root <guillaume.lelouet@gmail.com>
2014-10-06 11:33:38 +0200

    corrected error in code

--- optiplace-core/src/main/java/fr/emn/optiplace/SolvingProcess.java
+++ optiplace-core/src/main/java/fr/emn/optiplace/SolvingProcess.java
@@ -18,6 +18,7 @@ import solver.search.solution.Solution;
 import solver.search.strategy.IntStrategyFactory;
 import solver.search.strategy.strategy.AbstractStrategy;
 import solver.variables.IntVar;
+import solver.variables.Variable;
 import fr.emn.optiplace.actions.Migrate;
 import fr.emn.optiplace.configuration.Configuration;
 import fr.emn.optiplace.configuration.resources.ResourceHandler;
@@ -42,204 +43,205 @@ import fr.emn.optiplace.view.ViewAsModule;
  */
 public class SolvingProcess extends OptiplaceProcess {
 
-	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory
-			.getLogger(SolvingProcess.class);
+  private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory
+      .getLogger(SolvingProcess.class);
 
-	/** the core problem, modified by the views */
-	protected DefaultReconfigurationProblem problem;
+  /** the core problem, modified by the views */
+  protected DefaultReconfigurationProblem problem;
 
-	ArrayList<ViewAsModule> views = null;
+  ArrayList<ViewAsModule> views = null;
 
-	@Override
-	public void makeProblem() {
-		long st = System.currentTimeMillis();
-		Configuration src = center.getSource();
-		problem = new DefaultReconfigurationProblem(src);
-		if (strat.getPacker() == null) {
-			strat.setPacker(new FastBinPacker());
-		}
+  @Override
+  public void makeProblem() {
+    long st = System.currentTimeMillis();
+    Configuration src = center.getSource();
+    problem = new DefaultReconfigurationProblem(src);
+    if (strat.getPacker() == null) {
+      strat.setPacker(new FastBinPacker());
+    }
 
-		// if we have a view specified by the administrator (containing rules,
-		// objectives, etc.) then we add it at the end.
-		views = center.getViews();
-		if (center.getBaseView() != null) {
-			views = new ArrayList<>(views);
-			views.add(center.getBaseView());
-		}
+    // if we have a view specified by the administrator (containing rules,
+    // objectives, etc.) then we add it at the end.
+    views = center.getViews();
+    if (center.getBaseView() != null) {
+      views = new ArrayList<>(views);
+      views.add(center.getBaseView());
+    }
 
-		for (ResourceSpecification r : src.resources().values()) {
-			problem.addResourceHandler(new ResourceHandler(r));
-		}
+    for (ResourceSpecification r : src.resources().values()) {
+      problem.addResourceHandler(new ResourceHandler(r));
+    }
 
-		// add all the resources specified by the view
-		for (ViewAsModule v : views) {
-			for (ResourceSpecification r : v.getPackedResource()) {
-				if (r != null) {
-					problem.addResourceHandler(new ResourceHandler(r));
-				}
-			}
-		}
-		for (ViewAsModule view : views) {
-			view.associate(problem);
-		}
-		ChocoResourcePacker packer = strat.getPacker();
-		// all the resources should be added now, we pack them using the packing
-		// constraint.
-		for (Constraint c : packer.pack(problem.getSolver().getEnvironment(),
-				problem.getHosters(), problem.getUses())) {
-			problem.getSolver().post(c);
-		}
-		for (ViewAsModule view : views) {
-			for (Rule cc : view.getRequestedRules()) {
-				cc.inject(problem);
-			}
-		}
+    // add all the resources specified by the view
+    for (ViewAsModule v : views) {
+      for (ResourceSpecification r : v.getPackedResource()) {
+        if (r != null) {
+          problem.addResourceHandler(new ResourceHandler(r));
+        }
+      }
+    }
+    for (ViewAsModule view : views) {
+      view.associate(problem);
+    }
+    ChocoResourcePacker packer = strat.getPacker();
+    // all the resources should be added now, we pack them using the packing
+    // constraint.
+    for (Constraint c : packer.pack(problem.getSolver().getEnvironment(),
+        problem.getHosters(), problem.getUses())) {
+      problem.getSolver().post(c);
+    }
+    for (ViewAsModule view : views) {
+      for (Rule cc : view.getRequestedRules()) {
+        cc.inject(problem);
+      }
+    }
 
-		target.setBuildTime(System.currentTimeMillis() - st);
-		target.setProblem(problem);
-	}
+    target.setBuildTime(System.currentTimeMillis() - st);
+    target.setProblem(problem);
+  }
 
-	@Override
-	public void configLogging() {
-		strat.getDisplayers().forEach(problem.getSolver()::plugMonitor);
-	}
+  @Override
+  public void configLogging() {
+    strat.getDisplayers().forEach(problem.getSolver()::plugMonitor);
+  }
 
-	@SuppressWarnings("rawtypes")
-	@Override
-	public void configSearch() {
-		long st = System.currentTimeMillis();
+  @SuppressWarnings("rawtypes")
+  @Override
+  public void configSearch() {
+    long st = System.currentTimeMillis();
 
-		// get the goal if any
-		SearchGoal goalMaker = null;
-		for (ViewAsModule v : views) {
-			SearchGoal sg = v.getSearchGoal();
-			if (sg != null) {
-				goalMaker = sg;
-			}
-		}
-		if (goalMaker == null) {
-			ResourceHandler handler = problem.getResourcesHandlers().get("MEM");
-			if (handler != null) {
-				goalMaker = new MigrationReducerGoal(handler.getSpecs());
-			} else {
-				goalMaker = NBMigrationsCost.INSTANCE;
-			}
+    // get the goal if any
+    SearchGoal goalMaker = null;
+    for (ViewAsModule v : views) {
+      SearchGoal sg = v.getSearchGoal();
+      if (sg != null) {
+        goalMaker = sg;
+      }
+    }
+    if (goalMaker == null) {
+      ResourceHandler handler = problem.getResourcesHandlers().get("MEM");
+      if (handler != null) {
+        goalMaker = new MigrationReducerGoal(handler.getSpecs());
+      } else {
+        goalMaker = NBMigrationsCost.INSTANCE;
+      }
 
-		}
-		problem.getSolver().set(
-				new ObjectiveManager(goalMaker.getObjective(problem),
-						ResolutionPolicy.MINIMIZE, true));
+    }
+    problem.getSolver().set(
+        new ObjectiveManager(goalMaker.getObjective(problem),
+            ResolutionPolicy.MINIMIZE, true));
 
-		// add all the heuristics.
-		// first heuristic : the global goal heuristic
-		SearchHeuristic[] objectiveHeuristics = goalMaker != null ? goalMaker
-				.getHeuristics() : null;
-		ArrayList<SearchHeuristic> heuristicsGenerators = new ArrayList<SearchHeuristic>();
-		if (objectiveHeuristics != null) {
-			heuristicsGenerators.addAll(Arrays.asList(objectiveHeuristics));
-		}
-		// then add all heuristics from the view, in the views reverse order.
-		for (int i = views.size() - 1; i >= 0; i--) {
-			List<SearchHeuristic> viewGens = views.get(i).getSearchHeuristics();
-			heuristicsGenerators.addAll(viewGens);
-		}
-		// then add default heuristics.
-		if (problem.getResourcesHandlers().get("MEM") != null) {
-			heuristicsGenerators.add(new StickVMsHeuristic(problem
-					.getResourcesHandlers().get("MEM").getSpecs()));
-		}
-		heuristicsGenerators.add(DummyPlacementHeuristic.INSTANCE);
+    // add all the heuristics.
+    // first heuristic : the global goal heuristic
+    SearchHeuristic[] objectiveHeuristics = goalMaker != null ? goalMaker
+        .getHeuristics() : null;
+        ArrayList<SearchHeuristic> heuristicsGenerators = new ArrayList<SearchHeuristic>();
+        if (objectiveHeuristics != null) {
+          heuristicsGenerators.addAll(Arrays.asList(objectiveHeuristics));
+        }
+        // then add all heuristics from the view, in the views reverse order.
+        for (int i = views.size() - 1; i >= 0; i--) {
+          List<SearchHeuristic> viewGens = views.get(i).getSearchHeuristics();
+          heuristicsGenerators.addAll(viewGens);
+        }
+        // then add default heuristics.
+        if (problem.getResourcesHandlers().get("MEM") != null) {
+          heuristicsGenerators.add(new StickVMsHeuristic(problem
+              .getResourcesHandlers().get("MEM").getSpecs()));
+        }
+        heuristicsGenerators.add(DummyPlacementHeuristic.INSTANCE);
 
-		// all the heuristics are generated and added in the problem here.
-		List<AbstractStrategy<IntVar>> strats = heuristicsGenerators.stream()
-				.map(sh -> sh.getHeuristics(problem)).flatMap(l -> l.stream())
-				.collect(Collectors.toList());
-		problem.getSolver().set(
-				IntStrategyFactory.sequencer(strats.toArray(new AbstractStrategy[0])));
+        // all the heuristics are generated and added in the problem here.
+    List<AbstractStrategy<? extends Variable>> strats = heuristicsGenerators
+        .stream()
+            .map(sh -> sh.getHeuristics(problem)).flatMap(l -> l.stream())
+            .collect(Collectors.toList());
+        problem.getSolver().set(
+            IntStrategyFactory.sequencer(strats.toArray(new AbstractStrategy[0])));
 
-		if (strat.getMaxSearchTime() > 0) {
-			SearchMonitorFactory.limitTime(problem.getSolver(),
-					strat.getMaxSearchTime());
-		}
+        if (strat.getMaxSearchTime() > 0) {
+          SearchMonitorFactory.limitTime(problem.getSolver(),
+              strat.getMaxSearchTime());
+        }
 
-		target.setConfigTime(System.currentTimeMillis() - st);
-	}
+        target.setConfigTime(System.currentTimeMillis() - st);
+  }
 
-	@Override
-	public void makeSearch() {
-		long st = System.currentTimeMillis();
-		if (problem.getSolver().getObjectiveManager().getObjective() == null
-				|| strat.getReducer() == null
-				|| strat.getReducer() == ObjectiveReducer.IDENTITY) {
-			problem.getSolver().findSolution();
-		} else {
-			problem.getSolver().findAllSolutions();
-		}
-		target.setSearchTime(System.currentTimeMillis() - st);
-	}
+  @Override
+  public void makeSearch() {
+    long st = System.currentTimeMillis();
+    if (problem.getSolver().getObjectiveManager().getObjective() == null
+        || strat.getReducer() == null
+        || strat.getReducer() == ObjectiveReducer.IDENTITY) {
+      problem.getSolver().findSolution();
+    } else {
+      problem.getSolver().findAllSolutions();
+    }
+    target.setSearchTime(System.currentTimeMillis() - st);
+  }
 
-	/**
+  /**
    *
    */
-	// Commented to pass from choc 2 to choco 3
-	// TODO implement this in choco 3
-	// private void searchAndReduce() {
-	// BranchAndBound bb = (BranchAndBound) problem.getSearchStrategy();
-	// MinIntObjManager obj = (MinIntObjManager) bb.getObjectiveManager();
-	// Field f = null;
-	// try {
-	// f = IntObjectiveManager.class.getDeclaredField("targetBound");
-	// f.setAccessible(true);
-	// } catch (Exception e) {
-	// logger.warn("while getting the field", e);
-	// }
-	// problem.launch();
-	// Solution s = null;
-	// if (problem.isFeasible() == Boolean.TRUE) {
-	// do {
-	// int objVal = ((IntVar) problem.getObjective()).getVal();
-	// s = problem.getSearchStrategy().getSolutionPool().getBestSolution();
-	// int newMax = (int) Math.ceil(strat.getReducer().reduce(objVal)) - 1;
-	// if (f != null) {
-	// try {
-	// f.set(obj, new Integer(newMax));
-	// } catch (Exception e) {
-	// logger.warn("", e);
-	// }
-	// }
-	// } while (problem.nextSolution() == Boolean.TRUE);
-	// } else {
-	// return;
-	// }
-	// problem.worldPopUntil(problem.getSearchStrategy().baseWorld);
-	// problem.restoreSolution(s);
-	// }
+  // Commented to pass from choc 2 to choco 3
+  // TODO implement this in choco 3
+  // private void searchAndReduce() {
+  // BranchAndBound bb = (BranchAndBound) problem.getSearchStrategy();
+  // MinIntObjManager obj = (MinIntObjManager) bb.getObjectiveManager();
+  // Field f = null;
+  // try {
+  // f = IntObjectiveManager.class.getDeclaredField("targetBound");
+  // f.setAccessible(true);
+  // } catch (Exception e) {
+  // logger.warn("while getting the field", e);
+  // }
+  // problem.launch();
+  // Solution s = null;
+  // if (problem.isFeasible() == Boolean.TRUE) {
+  // do {
+  // int objVal = ((IntVar) problem.getObjective()).getVal();
+  // s = problem.getSearchStrategy().getSolutionPool().getBestSolution();
+  // int newMax = (int) Math.ceil(strat.getReducer().reduce(objVal)) - 1;
+  // if (f != null) {
+  // try {
+  // f.set(obj, new Integer(newMax));
+  // } catch (Exception e) {
+  // logger.warn("", e);
+  // }
+  // }
+  // } while (problem.nextSolution() == Boolean.TRUE);
+  // } else {
+  // return;
+  // }
+  // problem.worldPopUntil(problem.getSearchStrategy().baseWorld);
+  // problem.restoreSolution(s);
+  // }
 
-	@Override
-	public void extractData() {
-		Solution s = problem.getSolver().getSolutionRecorder().getLastSolution();
-		IMeasures m = problem.getSolver().getMeasures();
-		if (s != null) {
-			try {
-				problem.getSolver().getSearchLoop().restoreRootNode();
-				s.restore();
-			} catch (ContradictionException e) {
-				logger.warn("", e);
-			}
-			target.setDestination(problem.extractConfiguration());
-			target.setObjective(((IntVar) problem.getSolver().getObjectiveManager()
-					.getObjective()).getValue());
-			Migrate.extractMigrations(center.getSource(), target.getDestination(),
-					target.getActions());
-			for (ViewAsModule v : center.getViews()) {
-				v.endSolving(target.getActions());
-			}
-		} else {
-			target.setDestination(null);
-		}
-		target.setSearchBacktracks(m.getBackTrackCount());
-		target.setSearchNodes(m.getNodeCount());
-		target.setSearchSolutions(problem.getSolutionRecorder().getSolutions()
-				.size());
-	}
+  @Override
+  public void extractData() {
+    Solution s = problem.getSolver().getSolutionRecorder().getLastSolution();
+    IMeasures m = problem.getSolver().getMeasures();
+    if (s != null) {
+      try {
+        problem.getSolver().getSearchLoop().restoreRootNode();
+        s.restore();
+      } catch (ContradictionException e) {
+        logger.warn("", e);
+      }
+      target.setDestination(problem.extractConfiguration());
+      target.setObjective(((IntVar) problem.getSolver().getObjectiveManager()
+          .getObjective()).getValue());
+      Migrate.extractMigrations(center.getSource(), target.getDestination(),
+          target.getActions());
+      for (ViewAsModule v : center.getViews()) {
+        v.endSolving(target.getActions());
+      }
+    } else {
+      target.setDestination(null);
+    }
+    target.setSearchBacktracks(m.getBackTrackCount());
+    target.setSearchNodes(m.getNodeCount());
+    target.setSearchSolutions(problem.getSolutionRecorder().getSolutions()
+        .size());
+  }
 }
