ab05664a88ca4691f7a54ba58b016875e22b8490
Guillaume <guillaume.lelouet@gmail.com>
2014-10-22 19:18:06 +0200

    removed boolean value of clean on update; now onNewConfig is empty, to redefine if needed

--- optiplace-API/src/main/java/fr/emn/optiplace/view/ProvidedDataReader.java
+++ optiplace-API/src/main/java/fr/emn/optiplace/view/ProvidedDataReader.java
@@ -9,31 +9,15 @@ package fr.emn.optiplace.view;
  */
 public interface ProvidedDataReader {
 
-  /** @return whether or not clean() should be called before loading new
-   * configuration files, default is false; <br />
-   * if this returns true, {@link #clean()} should also be redefined */
-  default boolean cleanOnRead() {
-    return false;
+	/** called before a new config is read. */
+	default void onNewConfig() {
   }
 
   default void read(ProvidedData conf) {
-    read(conf, cleanOnRead());
-  }
-
-  default void read(ProvidedData conf, boolean clean) {
-    if (clean) {
-      clean();
-    }
+		onNewConfig();
     conf.lines().forEach(this::readLine);
   }
 
   void readLine(String line);
-
-  /** clean the internal data, called before reading the lines of a providedData
-   * if {@link #cleanOnRead()} returns true<br />
-   * clean() should be redefined when cleanOnRead() returns true; */
-  default void clean() {
-
-  }
 
 }
