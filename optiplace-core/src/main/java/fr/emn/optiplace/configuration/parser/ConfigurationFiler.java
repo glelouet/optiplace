cf9336a1f1602a1f354a9e7e35d8bda7cdfb33ec
root <guillaume.lelouet@gmail.com>
2014-10-31 18:11:26 +0100

    reworked Config filer

--- optiplace-core/src/main/java/fr/emn/optiplace/configuration/parser/ConfigurationFiler.java
+++ optiplace-core/src/main/java/fr/emn/optiplace/configuration/parser/ConfigurationFiler.java
@@ -3,123 +3,124 @@
  */
 package fr.emn.optiplace.configuration.parser;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
 
 import fr.emn.optiplace.configuration.Configuration;
 import fr.emn.optiplace.configuration.Node;
 import fr.emn.optiplace.configuration.SimpleConfiguration;
 import fr.emn.optiplace.configuration.VM;
+import fr.emn.optiplace.configuration.resources.MappedResourceSpecification;
 
 /**
  * @author Guillaume Le Louët [guillaume.lelouet@gmail.com]2014
  */
 public class ConfigurationFiler {
 
-	@SuppressWarnings("unused")
-	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory
-			.getLogger(ConfigurationFiler.class);
+    @SuppressWarnings("unused")
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ConfigurationFiler.class);
 
-	File file;
+    File file;
 
-	Configuration cfg = new SimpleConfiguration();
+    Configuration cfg = new SimpleConfiguration();
 
-	/**
-	 *
-	 */
-	public ConfigurationFiler(File file) {
-		this.file = file;
+    /**
+     *
+     */
+    public ConfigurationFiler(File file) {
+	this.file = file;
+    }
+
+    public ConfigurationFiler withConfiguration(Configuration cfg) {
+	this.cfg = cfg;
+	return this;
+    }
+
+    public Configuration getCfg() {
+	return cfg;
+    }
+
+    public void read() {
+	try {
+	    new BufferedReader(new FileReader(file)).lines().forEach(this::readLine);
+	} catch (Exception e) {
+	    throw new UnsupportedOperationException(e);
 	}
+    }
 
-	public ConfigurationFiler withConfiguration(Configuration cfg) {
-		this.cfg = cfg;
-		return this;
-	}
-
-	public void read() {
-		try {
-			new BufferedReader(new FileReader(file)).lines().forEach(this::readLine);
-		} catch (Exception e) {
-			throw new UnsupportedOperationException(e);
-		}
-	}
-
-	protected void readLine(String line) {
-		ArrayList<Character> buffer = new ArrayList<>();
-		String nodeName = null;
-		boolean escaped = false;
-		boolean online = false;
-		ArrayList<String> vms = new ArrayList<>();
-		for (char c : line.toCharArray()) {
-			if (escaped) {
-				buffer.add(c);
-				escaped = false;
-			} else {
-				switch (c) {
-				case '\\':
-					escaped = true;
-					break;
-				case ' ':
-					if (nodeName == null) {
-						nodeName = String.valueOf(buffer.toArray());
-					} else {
-						if (buffer.size() > 0) {
-							vms.add(String.valueOf(buffer.toArray()));
-						}
-					}
-					buffer.clear();
-					break;
-				case ':':
-					online = true;
-				default:
-					buffer.add(c);
-				}
+    protected void readLine(String line) {
+	if (line.startsWith("onlines : {")) {
+	    line = line.substring("onlines : {".length(), line.length() - 2);
+	    String[] lines = line.split("], ");
+	    for (String l2 : lines) {
+		String[] l2s = l2.split("=\\[");
+		String nodeName = l2s[0];
+		if (nodeName.length() != 0) {
+		    Node n = cfg.addOnline(nodeName);
+		    if (l2s.length > 1) {
+			String[] vms = l2s[1].split(", ");
+			for (String vm : vms) {
+			    cfg.addVM(vm, n);
 			}
+		    }
+		} else {
+		    System.err.println("node name null : " + nodeName);
 		}
-		if (nodeName != null && buffer.size() > 0) {
-			vms.add(String.valueOf(buffer.toArray()));
-		}
-		if (nodeName == null) {
-			return;
-		}
-		Node n = new Node(nodeName);
-		if (online) {
-			cfg.setOnline(n);
-			for (String s : vms) {
-				cfg.setHost(new VM(s), n);
-			}
-		}
-	}
+	    }
+	} else if (line.startsWith("offlines : [")) {
+	    line = line.substring("offlines : [".length(), line.length() - 1);
+	    String[] lines = line.split(", ");
+	    for (String s : lines) {
+		cfg.addOffline(s);
+	    }
+	} else if (line.startsWith("waitings : [")) {
+	    line = line.substring("waitings : [".length(), line.length() - 1);
+	    String[] lines = line.split(", ");
+	    for (String s : lines) {
+		cfg.addVM(s, null);
+	    }
 
-
-	public void write() {
-		try {
-			BufferedWriter b = new BufferedWriter(new FileWriter(file));
-			cfg.getOnlines().forEach(n -> {
-				write(b, escapeElement(n.getName()) + " :");
-					cfg.getHosted(n).forEach(v -> {
-					write(b, " " + escapeElement(v.getName()));
-					});
-			});
-		} catch (IOException e) {
-			throw new UnsupportedOperationException(e);
-		}
+	} else if (line.startsWith(" ")) {
+	    line = line.substring(" ".length());
+	    String[] l2s = line.split("\\{");
+	    String resName = l2s[0];
+	    MappedResourceSpecification res = new MappedResourceSpecification(resName);
+	    cfg.resources().put(resName, res);
+	    String[] nodes = l2s[1].substring(0, l2s[1].length() - 1).split(", ");
+	    for (String r : nodes) {
+		String[] rs = r.split("=");
+		res.toCapacities().put(new Node(rs[0]), Integer.parseInt(rs[1]));
+	    }
+	    String[] vms = l2s[2].substring(0, l2s[2].length() - 1).split(", ");
+	    for (String v : vms) {
+		String[] vs = v.split("=");
+		res.toUses().put(new VM(vs[0]), Integer.parseInt(vs[1]));
+	    }
 	}
+    }
 
-	protected static void write(BufferedWriter b, String s) {
-		try {
-			b.write(s);
-		} catch (IOException e) {
-			throw new UnsupportedOperationException(e);
-		}
+    public void write() {
+	try {
+	    BufferedWriter b = new BufferedWriter(new FileWriter(file));
+	    cfg.getOnlines().forEach(n -> {
+		write(b, escapeElement(n.getName()) + " :");
+		cfg.getHosted(n).forEach(v -> {
+		    write(b, " " + escapeElement(v.getName()));
+		});
+	    });
+	} catch (IOException e) {
+	    throw new UnsupportedOperationException(e);
 	}
+    }
 
-	protected String escapeElement(String elementName) {
-		return elementName.replaceAll(" ", "\\ ").replaceAll(":", "\\:");
+    protected static void write(BufferedWriter b, String s) {
+	try {
+	    b.write(s);
+	} catch (IOException e) {
+	    throw new UnsupportedOperationException(e);
 	}
+    }
+
+    protected String escapeElement(String elementName) {
+	return elementName.replaceAll(" ", "\\ ").replaceAll(":", "\\:");
+    }
 }
