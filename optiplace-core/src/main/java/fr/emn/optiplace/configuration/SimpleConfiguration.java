fde8e26b4ec0404c1eb2078f305151f2b9dbac99
Guillaume Le Louët <guillaume.lelouet@gmail.com>
2015-05-12 18:02:48 +0200

    added migration status of VM to Configuration

--- optiplace-core/src/main/java/fr/emn/optiplace/configuration/SimpleConfiguration.java
+++ optiplace-core/src/main/java/fr/emn/optiplace/configuration/SimpleConfiguration.java
@@ -10,12 +10,8 @@
 
 package fr.emn.optiplace.configuration;
 
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -25,6 +21,7 @@ import fr.emn.optiplace.center.configuration.VM;
 import fr.emn.optiplace.center.configuration.resources.ResourceSpecification;
 import fr.emn.optiplace.configuration.parser.ConfigurationFiler;
 import fr.emn.optiplace.configuration.resources.MappedResourceSpecification;
+
 
 /**
  * Default implementation of Configuration. The elements are stored in
@@ -37,324 +34,343 @@ import fr.emn.optiplace.configuration.resources.MappedResourceSpecification;
  */
 public class SimpleConfiguration implements Configuration {
 
-    private final Set<Node> offlines = new LinkedHashSet<>();
+	private final Set<Node> offlines = new LinkedHashSet<>();
 
-    private final Map<Node, Set<VM>> hosted = new LinkedHashMap<>();
+	private final Map<Node, Set<VM>> hosted = new LinkedHashMap<>();
 
-    private final Set<VM> waitings = new LinkedHashSet<>();
+	private final Set<VM> waitings = new LinkedHashSet<>();
 
-    private final Map<VM, Node> vmLocs = new LinkedHashMap<>();
+	private final Map<VM, Node> vmLocs = new LinkedHashMap<>();
 
-    public SimpleConfiguration(String... resources) {
-	if (resources == null || resources.length == 0) {
-	} else {
-	    for (String r : resources) {
-		this.resources.put(r, new MappedResourceSpecification(r));
-	    }
+	private final Map<VM, Node> migrations = new LinkedHashMap<>();
+
+	public SimpleConfiguration(String... resources) {
+		if (resources == null || resources.length == 0) {} else {
+			for (String r : resources) {
+				this.resources.put(r, new MappedResourceSpecification(r));
+			}
+		}
 	}
-    }
 
-    protected LinkedHashMap<String, ResourceSpecification> resources = new LinkedHashMap<String, ResourceSpecification>();
+	protected LinkedHashMap<String, ResourceSpecification> resources = new LinkedHashMap<String, ResourceSpecification>();
 
-    @Override
-    public LinkedHashMap<String, ResourceSpecification> resources() {
-	return resources;
-    }
-
-    @Override
-    public Stream<Node> getOnlines() {
-	return hosted.keySet().stream();
-    }
-
-    @Override
-    public Stream<Node> getOfflines() {
-	return offlines.stream();
-    }
-
-    @Override
-    public int nbNodes(NODESTATES state) {
-	if (state == null) {
-	    return offlines.size() + hosted.size();
+	@Override
+	public LinkedHashMap<String, ResourceSpecification> resources() {
+		return resources;
 	}
-	switch (state) {
-	case OFFLINE:
-	    return offlines.size();
-	case ONLINE:
-	    return hosted.size();
-	default:
-	    throw new UnsupportedOperationException();
+
+	@Override
+	public Stream<Node> getOnlines() {
+		return hosted.keySet().stream();
 	}
-    }
 
-    @Override
-    public Stream<VM> getRunnings() {
-	return vmLocs.keySet().stream();
-    }
-
-    @Override
-    public Stream<VM> getWaitings() {
-	return waitings.stream();
-    }
-
-    @Override
-    public int nbVMs(VMSTATES state) {
-	if (state == null) {
-	    return vmLocs.size() + waitings.size();
+	@Override
+	public Stream<Node> getOfflines() {
+		return offlines.stream();
 	}
-	switch (state) {
-	case RUNNING:
-	    return vmLocs.size();
-	case WAITING:
-	    return waitings.size();
-	default:
-	    throw new UnsupportedOperationException();
+
+	@Override
+	public int nbNodes(NODESTATES state) {
+		if (state == null) {
+			return offlines.size() + hosted.size();
+		}
+		switch (state) {
+			case OFFLINE:
+				return offlines.size();
+			case ONLINE:
+				return hosted.size();
+			default:
+				throw new UnsupportedOperationException();
+		}
 	}
-    }
 
-    @Override
-    public int nbHosted(Node host) {
-	Set<VM> vms = hosted.get(host);
-	return vms == null ? 0 : vms.size();
-    }
-
-    @Override
-    public boolean isOnline(Node n) {
-	return hosted.containsKey(n);
-    }
-
-    @Override
-    public boolean isOffline(Node n) {
-	return offlines.contains(n);
-    }
-
-    @Override
-    public boolean isRunning(VM vm) {
-	return vmLocs.containsKey(vm);
-    }
-
-    @Override
-    public boolean isWaiting(VM vm) {
-	return waitings.contains(vm);
-    }
-
-    @Override
-    public boolean setHost(VM vm, Node node2) {
-	if (vm == null || node2 == null || node2.equals(vmLocs.get(vm))) {
-	    return false;
+	@Override
+	public Stream<VM> getRunnings() {
+		return vmLocs.keySet().stream();
 	}
-	setOnline(node2);
-	waitings.remove(vm);
-	vmLocs.put(vm, node2);
-	hosted.get(node2).add(vm);
-	return true;
-    }
 
-    @Override
-    public boolean setWaiting(VM vm) {
-	if (isWaiting(vm)) {
-	    return false;
+	@Override
+	public Stream<VM> getWaitings() {
+		return waitings.stream();
 	}
-	Node hoster = vmLocs.remove(vm);
-	if (hoster != null) {
-	    hosted.get(hoster).remove(vm);
-	}
-	waitings.add(vm);
-	return true;
-    }
 
-    @Override
-    public VM addVM(String vmName, Node host, int... resources) {
-	VM vm = new VM(vmName);
-	if (host == null) {
-	    // we requested VM to be waiting.
-	    setWaiting(vm);
-	} else {
-	    setHost(vm, host);
+	@Override
+	public int nbVMs(VMSTATES state) {
+		if (state == null) {
+			return vmLocs.size() + waitings.size();
+		}
+		switch (state) {
+			case RUNNING:
+				return vmLocs.size();
+			case WAITING:
+				return waitings.size();
+			default:
+				throw new UnsupportedOperationException();
+		}
 	}
-	if (resources != null && resources.length > 0) {
-	    ResourceSpecification[] specs = this.resources.values().toArray(
-		    new ResourceSpecification[this.resources.size()]);
-	    for (int i = 0; i < specs.length && i < resources.length; i++) {
-		specs[i].toUses().put(vm, resources[i]);
-	    }
-	}
-	return vm;
-    }
 
-    @Override
-    public boolean remove(VM vm) {
-	if (waitings.remove(vm)) {
-	    return true;
+	@Override
+	public int nbHosted(Node host) {
+		Set<VM> vms = hosted.get(host);
+		return vms == null ? 0 : vms.size();
 	}
-	Node hoster = vmLocs.remove(vm);
-	if (hoster != null) {
-	    hosted.get(hoster).remove(vm);
-	    return true;
-	}
-	return false;
-    }
 
-    @Override
-    public boolean setOnline(Node node2) {
-	if (isOnline(node2)) {
-	    return false;
+	@Override
+	public boolean isOnline(Node n) {
+		return hosted.containsKey(n);
 	}
-	offlines.remove(node2.getName());
-	hosted.put(node2, new LinkedHashSet<>());
-	return true;
-    }
 
-    @Override
-    public Node addOnline(String name, int... resources) {
-	Node n = new Node(name);
-	setOnline(n);
-	if (resources != null && resources.length > 0) {
-	    ResourceSpecification[] specs = this.resources.values().toArray(
-		    new ResourceSpecification[this.resources.size()]);
-	    for (int i = 0; i < specs.length && i < resources.length; i++) {
-		specs[i].toCapacities().put(n, resources[i]);
-	    }
+	@Override
+	public boolean isOffline(Node n) {
+		return offlines.contains(n);
 	}
-	return n;
-    }
 
-    @Override
-    public Node addOffline(String name, int... resources) {
-	Node ret = addOnline(name, resources);
-	setOffline(ret);
-	return ret;
-    }
-
-    @Override
-    public boolean setOffline(Node node2) {
-	if (isOffline(node2)) {
-	    return false;
+	@Override
+	public boolean isRunning(VM vm) {
+		return vmLocs.containsKey(vm);
 	}
-	Set<VM> vms = hosted.remove(node2);
-	if (vms != null) {
-	    for (VM vm : vms) {
-		vmLocs.remove(vm);
+
+	@Override
+	public boolean isWaiting(VM vm) {
+		return waitings.contains(vm);
+	}
+
+	@Override
+	public boolean setHost(VM vm, Node node2) {
+		if (vm == null || node2 == null || node2.equals(vmLocs.get(vm))) {
+			return false;
+		}
+		setOnline(node2);
+		waitings.remove(vm);
+		vmLocs.put(vm, node2);
+		hosted.get(node2).add(vm);
+		return true;
+	}
+
+	@Override
+	public boolean setWaiting(VM vm) {
+		if (isWaiting(vm)) {
+			return false;
+		}
+		Node hoster = vmLocs.remove(vm);
+		if (hoster != null) {
+			hosted.get(hoster).remove(vm);
+		}
 		waitings.add(vm);
-	    }
+		return true;
 	}
-	offlines.add(node2);
-	return true;
-    }
 
-    @Override
-    public boolean remove(Node n) {
-	// if node offline : remove it from offlines, then it's ok
-	if (offlines.remove(n)) {
-	    return true;
+	/**
+	 *
+	 */
+	@Override
+	public Node getMigrationTarget(VM v) {
+		return migrations.get(v);
 	}
-	Set<VM> vms = hosted.remove(n);
-	if (vms != null) {
-	    for (VM vm : vms) {
-		vmLocs.remove(vm);
-		waitings.add(vm);
-	    }
-	    return true;
-	}
-	return false;
-    }
 
-    @Override
-    public Stream<VM> getHosted(Node n) {
-	return hosted.get(n).stream();
+	@Override
+	public void setMigrationTarget(VM vm, Node n) {
+		if(vm==null || !vmLocs.containsKey(vm)) {
+	    return ;
     }
+		if (n == null || n.equals(vmLocs.get(vm))) {
+			migrations.remove(vm);
+		} else {
+			migrations.put(vm, n);
+		}
+	}
 
-    @Override
-    public Node getLocation(VM vm) {
-	return vmLocs.get(vm);
-    }
+	@Override
+	public VM addVM(String vmName, Node host, int... resources) {
+		VM vm = new VM(vmName);
+		if (host == null) {
+			// we requested VM to be waiting.
+			setWaiting(vm);
+		} else {
+			setHost(vm, host);
+		}
+		if (resources != null && resources.length > 0) {
+			ResourceSpecification[] specs = this.resources.values().toArray(new ResourceSpecification[this.resources.size()]);
+			for (int i = 0; i < specs.length && i < resources.length; i++) {
+				specs[i].toUses().put(vm, resources[i]);
+			}
+		}
+		return vm;
+	}
 
-    @Override
-    public void replace(VM vm, VM newVM) {
-	if (vm == newVM || vm == null || newVM == null) {
-	    return;
+	@Override
+	public boolean remove(VM vm) {
+		if (waitings.remove(vm)) {
+			return true;
+		}
+		Node hoster = vmLocs.remove(vm);
+		if (hoster != null) {
+			hosted.get(hoster).remove(vm);
+			return true;
+		}
+		return false;
 	}
-	Node hoster = vmLocs.remove(vm);
-	if (hoster == null) {
-	    if (waitings.remove(vm)) {
-		waitings.add(newVM);
-	    } else {
-		// the VM had no hoster and was not waiting : it was not
-		// present.
-	    }
-	} else {
-	    Set<VM> set = hosted.get(hoster);
-	    set.remove(vm);
-	    set.add(newVM);
-	    vmLocs.put(newVM, hoster);
-	}
-    }
 
-    @Override
-    public void replace(Node oldNode, Node newNode) {
-	if (oldNode == newNode || oldNode == null || newNode == null) {
-	    return;
+	@Override
+	public boolean setOnline(Node node2) {
+		if (isOnline(node2)) {
+			return false;
+		}
+		offlines.remove(node2.getName());
+		hosted.put(node2, new LinkedHashSet<>());
+		return true;
 	}
-	Set<VM> vms = hosted.remove(oldNode);
-	if (vms == null) {
-	    if (offlines.remove(oldNode)) {
-		offlines.add(newNode);
-	    } else {
-		// the node had a null set of VMs, and was not offline : was not
-		// present.
-	    }
-	} else {
-	    hosted.put(newNode, vms);
-	    for (VM vm : vms) {
-		vmLocs.put(vm, newNode);
-	    }
-	}
-    }
 
-    @Override
-    public Stream<Node> getOnlines(Predicate<Set<VM>> pred) {
-	return hosted.entrySet().stream().filter(e -> pred.test(Collections.unmodifiableSet(e.getValue())))
-		.map(Entry<Node, Set<VM>>::getKey);
-    }
+	@Override
+	public Node addOnline(String name, int... resources) {
+		Node n = new Node(name);
+		setOnline(n);
+		if (resources != null && resources.length > 0) {
+			ResourceSpecification[] specs = this.resources.values().toArray(new ResourceSpecification[this.resources.size()]);
+			for (int i = 0; i < specs.length && i < resources.length; i++) {
+				specs[i].toCapacities().put(n, resources[i]);
+			}
+		}
+		return n;
+	}
 
-    @Override
-    public String toString() {
-	return "onlines : " + hosted + "\nofflines : " + offlines + "\nwaitings : " + waitings + "\nresources : "
-		+ resources.entrySet().stream().map(e -> " " + e.getValue()).reduce("", (s, t) -> s + "\n" + t);
-    }
+	@Override
+	public Node addOffline(String name, int... resources) {
+		Node ret = addOnline(name, resources);
+		setOffline(ret);
+		return ret;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-	if (super.equals(obj)) {
-	    return true;
+	@Override
+	public boolean setOffline(Node node2) {
+		if (isOffline(node2)) {
+			return false;
+		}
+		Set<VM> vms = hosted.remove(node2);
+		if (vms != null) {
+			for (VM vm : vms) {
+				vmLocs.remove(vm);
+				waitings.add(vm);
+			}
+		}
+		offlines.add(node2);
+		return true;
 	}
-	if (obj == null || obj.getClass() != SimpleConfiguration.class) {
-	    return false;
+
+	@Override
+	public boolean remove(Node n) {
+		// if node offline : remove it from offlines, then it's ok
+		if (offlines.remove(n)) {
+			return true;
+		}
+		Set<VM> vms = hosted.remove(n);
+		if (vms != null) {
+			for (VM vm : vms) {
+				vmLocs.remove(vm);
+				waitings.add(vm);
+			}
+			return true;
+		}
+		return false;
 	}
-	SimpleConfiguration o = (SimpleConfiguration) obj;
-	if (!vmLocs.equals(o.vmLocs)) {
-	    // System.err.println("vmlocs : " + vmLocs + " != " + o.vmLocs);
-	    return false;
+
+	@Override
+	public Stream<VM> getHosted(Node n) {
+		return hosted.get(n).stream();
 	}
-	if (!offlines.equals(o.offlines)) {
-	    // System.err.println("offlines");
-	    return false;
+
+	@Override
+	public Node getLocation(VM vm) {
+		return vmLocs.get(vm);
 	}
-	if (!waitings.equals(o.waitings)) {
-	    // System.err.println("waitings");
-	    return false;
+
+	@Override
+	public void replace(VM vm, VM newVM) {
+		if (vm == newVM || vm == null || newVM == null) {
+			return;
+		}
+		Node hoster = vmLocs.remove(vm);
+		if (hoster == null) {
+			if (waitings.remove(vm)) {
+				waitings.add(newVM);
+			} else {
+				// the VM had no hoster and was not waiting : it was not
+				// present.
+			}
+		} else {
+			Set<VM> set = hosted.get(hoster);
+			set.remove(vm);
+			set.add(newVM);
+			vmLocs.put(newVM, hoster);
+		}
 	}
-	if (!resources.equals(o.resources)) {
-	    // System.err.println("resources : ");
-	    // for (Entry<String, ResourceSpecification> e :
-	    // resources.entrySet()) {
-	    // ResourceSpecification o2 = o.resources.get(e.getKey());
-	    // if (!e.getValue().equals(o2)) {
-	    // System.err.println(" " + e.getKey() + " : " + e.getValue() +
-	    // " != " + o2);
-	    // }
-	    // }
-	    return false;
+
+	@Override
+	public void replace(Node oldNode, Node newNode) {
+		if (oldNode == newNode || oldNode == null || newNode == null) {
+			return;
+		}
+		Set<VM> vms = hosted.remove(oldNode);
+		if (vms == null) {
+			if (offlines.remove(oldNode)) {
+				offlines.add(newNode);
+			} else {
+				// the node had a null set of VMs, and was not offline : was not
+				// present.
+			}
+		} else {
+			hosted.put(newNode, vms);
+			for (VM vm : vms) {
+				vmLocs.put(vm, newNode);
+			}
+		}
 	}
-	return true;
-    }
+
+	@Override
+	public Stream<Node> getOnlines(Predicate<Set<VM>> pred) {
+		return hosted.entrySet().stream().filter(e -> pred.test(Collections.unmodifiableSet(e.getValue())))
+		    .map(Entry<Node, Set<VM>>::getKey);
+	}
+
+	@Override
+	public String toString() {
+		return "onlines : " + hosted + "\nofflines : " + offlines + "\nwaitings : " + waitings + "\nresources : "
+		    + resources.entrySet().stream().map(e -> " " + e.getValue()).reduce("", (s, t) -> s + "\n" + t);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (super.equals(obj)) {
+			return true;
+		}
+		if (obj == null || obj.getClass() != SimpleConfiguration.class) {
+			return false;
+		}
+		SimpleConfiguration o = (SimpleConfiguration) obj;
+		if (!vmLocs.equals(o.vmLocs)) {
+			// System.err.println("vmlocs : " + vmLocs + " != " + o.vmLocs);
+			return false;
+		}
+		if (!offlines.equals(o.offlines)) {
+			// System.err.println("offlines");
+			return false;
+		}
+		if (!waitings.equals(o.waitings)) {
+			// System.err.println("waitings");
+			return false;
+		}
+		if (!resources.equals(o.resources)) {
+			// System.err.println("resources : ");
+			// for (Entry<String, ResourceSpecification> e :
+			// resources.entrySet()) {
+			// ResourceSpecification o2 = o.resources.get(e.getKey());
+			// if (!e.getValue().equals(o2)) {
+			// System.err.println(" " + e.getKey() + " : " + e.getValue() +
+			// " != " + o2);
+			// }
+			// }
+			return false;
+		}
+		return true;
+	}
 }
