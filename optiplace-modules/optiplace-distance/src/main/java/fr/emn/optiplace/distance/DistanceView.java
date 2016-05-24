8561bf0ef381095cccb92150e39a2e8fb28ef2ce
guillaume Le Louët <guillaume.lelouet@gmail.com>
2016-05-24 17:44:06 +0200

    updated versions and created starting point

---
+++ optiplace-modules/optiplace-distance/src/main/java/fr/emn/optiplace/distance/DistanceView.java
@@ -0,0 +1,7 @@
+package fr.emn.optiplace.distance;
+
+public class DistanceView {
+
+	@SuppressWarnings("unused")
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(DistanceView.class);
+}
